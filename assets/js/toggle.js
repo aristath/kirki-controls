2fe876be46d821e45f800d53cdb1c5077dcf9cfd
Aristeides Stathopoulos <aristath@gmail.com>
2017-08-12 18:19:23 +0300

    JS template for toggle controls

--- assets/js/toggle.js
+++ assets/js/toggle.js
@@ -1,14 +1,17 @@
 wp.customize.controlConstructor['kirki-toggle'] = wp.customize.kirkiDynamicControl.extend({
 
-	initKirkiControl: function() {
+	addHTML: function() {
 
 		var control = this,
-		    checkboxValue = control.setting._value;
+		    html = '';
 
-		// Save the value
-		this.container.on( 'change', 'input', function() {
-			checkboxValue = ( jQuery( this ).is( ':checked' ) ) ? true : false;
-			control.setting.set( checkboxValue );
-		});
+		html += '<label for="toggle_' + control.id + '">';
+			html += '<span class="customize-control-title">' + control.params.label + '</span>';
+			html += '<span class="description customize-control-description">' + control.params.description + '</span>';
+			html += '<input class="screen-reader-text" name="toggle_' + control.id + '" id="toggle_' + control.id + '" type="checkbox" value="' + control.params.value + '" ' + control.params.link + ( '1' === control.params.value ? ' checked' : '' ) + ' hidden />';
+			html += '<span class="switch"></span>';
+		html += '</label>';
+
+		control.container.html( html );
 	}
 });
--- classes/class-kirki-control-toggle.php
+++ classes/class-kirki-control-toggle.php
@@ -39,29 +39,4 @@ class Kirki_Control_Toggle extends Kirki_Control_Base {
 		$this->json['value'] = ( '1' === $this->json['value'] ) ? true : $this->json['value'];
 		$this->json['value'] = ( '0' === $this->json['value'] ) ? false : $this->json['value'];
 	}
-
-	/**
-	 * An Underscore (JS) template for this control's content (but not its container).
-	 *
-	 * Class variables for this control class are available in the `data` JS object;
-	 * export custom variables by overriding {@see WP_Customize_Control::to_json()}.
-	 *
-	 * @see WP_Customize_Control::print_template()
-	 *
-	 * @access protected
-	 */
-	protected function content_template() {
-		?>
-		<label for="toggle_{{ data.id }}">
-			<span class="customize-control-title">
-				{{{ data.label }}}
-			</span>
-			<# if ( data.description ) { #>
-				<span class="description customize-control-description">{{{ data.description }}}</span>
-			<# } #>
-			<input class="screen-reader-text" {{{ data.inputAttrs }}} name="toggle_{{ data.id }}" id="toggle_{{ data.id }}" type="checkbox" value="{{ data.value }}" {{{ data.link }}}<# if ( '1' == data.value ) { #> checked<# } #> hidden />
-			<span class="switch"></span>
-		</label>
-		<?php
-	}
 }
