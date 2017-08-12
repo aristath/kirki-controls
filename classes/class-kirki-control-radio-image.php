d58adbfa307ea50702a7569a9ba0d311a205da2b
Aristeides Stathopoulos <aristath@gmail.com>
2017-08-12 18:34:18 +0300

    js template for radio-image

--- classes/class-kirki-control-radio-image.php
+++ classes/class-kirki-control-radio-image.php
@@ -26,39 +26,4 @@ class Kirki_Control_Radio_Image extends Kirki_Control_Base {
 	 * @var string
 	 */
 	public $type = 'kirki-radio-image';
-
-	/**
-	 * An Underscore (JS) template for this control's content (but not its container).
-	 *
-	 * Class variables for this control class are available in the `data` JS object;
-	 * export custom variables by overriding {@see WP_Customize_Control::to_json()}.
-	 *
-	 * @see WP_Customize_Control::print_template()
-	 *
-	 * @access protected
-	 */
-	protected function content_template() {
-		?>
-		<label class="customizer-text">
-			<# if ( data.label ) { #><span class="customize-control-title">{{{ data.label }}}</span><# } #>
-			<# if ( data.description ) { #><span class="description customize-control-description">{{{ data.description }}}</span><# } #>
-		</label>
-		<div id="input_{{ data.id }}" class="image">
-			<# for ( key in data.choices ) { #>
-				<# dataAlt = ( _.isObject( data.choices[ key ] ) && ! _.isUndefined( data.choices[ key ].alt ) ) ? data.choices[ key ].alt : '' #>
-				<input {{{ data.inputAttrs }}} class="image-select" type="radio" value="{{ key }}" name="_customize-radio-{{ data.id }}" id="{{ data.id }}{{ key }}" {{{ data.link }}}<# if ( data.value === key ) { #> checked="checked"<# } #> data-alt="{{ dataAlt }}">
-					<label for="{{ data.id }}{{ key }}" {{{ data.labelStyle }}} class="{{{ data.id + key }}}">
-						<# if ( _.isObject( data.choices[ key ] ) ) { #>
-							<img src="{{ data.choices[ key ].src }}" alt="{{ data.choices[ key ].alt }}">
-							<span class="image-label"><span class="inner">{{ data.choices[ key ].alt }}</span></span>
-						<# } else { #>
-							<img src="{{ data.choices[ key ] }}">
-						<# } #>
-						<span class="image-clickable"></span>
-					</label>
-				</input>
-			<# } #>
-		</div>
-		<?php
-	}
 }
