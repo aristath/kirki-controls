fc4c20a1fc88982c3f8b3fee54e7fac9db9dc653
Aristeides Stathopoulos <aristath@gmail.com>
2017-08-13 03:01:00 +0300

    JS template for date control

--- assets/js/date.js
+++ assets/js/date.js
@@ -5,6 +5,8 @@ wp.customize.controlConstructor['kirki-date'] = wp.customize.kirkiDynamicControl
 		var control  = this,
 		    selector = control.selector + ' input.datepicker';
 
+		control.addHTML();
+
 		// Init the datepicker
 		jQuery( selector ).datepicker();
 
@@ -12,5 +14,22 @@ wp.customize.controlConstructor['kirki-date'] = wp.customize.kirkiDynamicControl
 		this.container.on( 'change keyup paste', 'input.datepicker', function() {
 			control.setting.set( jQuery( this ).val() );
 		});
+	},
+
+	addHTML: function() {
+		var control = this,
+		    html    = '',
+		    data    = control.params;
+
+		html += '<label>';
+			html += '<span class="customize-control-title">' + data.label + '</span>';
+			html += '<span class="description customize-control-description">' + data.description + '</span>';
+			html += '<div class="customize-control-content">';
+				html += '<input ' + data.inputAttrs + ' class="datepicker" type="text" id="' + data.id + '" value="' + data.value + '" ' + data.link + '/>';
+			html += '</div>';
+		html += '</label>';
+
+		control.container.html( html );
+
 	}
 });
--- classes/class-kirki-control-date.php
+++ classes/class-kirki-control-date.php
@@ -37,26 +37,4 @@ class Kirki_Control_Date extends Kirki_Control_Base {
 	protected function kirki_script_dependencies() {
 		return array( 'jquery-ui-datepicker' );
 	}
-
-	/**
-	 * An Underscore (JS) template for this control's content (but not its container).
-	 *
-	 * Class variables for this control class are available in the `data` JS object;
-	 * export custom variables by overriding {@see WP_Customize_Control::to_json()}.
-	 *
-	 * @see WP_Customize_Control::print_template()
-	 *
-	 * @access protected
-	 */
-	protected function content_template() {
-		?>
-		<label>
-			<# if ( data.label ) { #><span class="customize-control-title">{{{ data.label }}}</span><# } #>
-			<# if ( data.description ) { #><span class="description customize-control-description">{{{ data.description }}}</span><# } #>
-			<div class="customize-control-content">
-				<input {{{ data.inputAttrs }}} class="datepicker" type="text" id="{{ data.id }}" value="{{ data.value }}" {{{ data.link }}} />
-			</div>
-		</label>
-		<?php
-	}
 }
