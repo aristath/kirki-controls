467c2576a7f6b9fcb89a227c101d1e9b6449261b
Aristeides Stathopoulos <aristath@gmail.com>
2017-08-13 04:19:42 +0300

    JS template for multicheck control

--- assets/js/multicheck.js
+++ assets/js/multicheck.js
@@ -4,6 +4,8 @@ wp.customize.controlConstructor['kirki-multicheck'] = wp.customize.kirkiDynamicC
 
 		var control = this;
 
+		control.addHTML();
+
 		// Save the value
 		control.container.on( 'change', 'input', function() {
 			var value = [],
@@ -20,5 +22,26 @@ wp.customize.controlConstructor['kirki-multicheck'] = wp.customize.kirkiDynamicC
 			// Update the value in the customizer.
 			control.setting.set( value );
 		});
+	},
+
+	addHTML: function() {
+
+		var control = this,
+		    html    = '';
+
+		if ( ! control.params.choices ) {
+			return;
+		}
+
+		html += '<span class="customize-control-title">' + control.params.label + '</span><# } #>';
+		html += '<span class="description customize-control-description">' + control.params.description + '</span>';
+
+		html += '<ul>';
+			_.each( control.params.choices, function( val, key ) {
+				html += '<li><label><input ' + control.params.inputAttrs + ' type="checkbox" value="' + key + '"' + ( _.contains( control.params.value, key ) ? ' checked' : '' ) + '/>' + val + '</label></li>';
+			});
+		html += '</ul>';
+
+		control.container.html( html );
 	}
 });
