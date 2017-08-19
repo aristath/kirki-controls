546bac9670e6823947d47100367ae7b08e4ecebf
Aristeides Stathopoulos <aristath@gmail.com>
2017-08-19 16:15:42 +0300

    multicolor control now extends dynamic control

--- js/multicolor.js
+++ js/multicolor.js
@@ -1,59 +1,23 @@
 /* global wp, _ */
 wp.customize.controlConstructor['kirki-multicolor'] = wp.customize.Control.extend( {
 
-	// When we're finished loading continue processing
-	ready: function() {
-		var control = this;
-
-		control.addHTML();
-
-		control.initKirkiControl();
-	},
-
 	initKirkiControl: function() {
 		var control = this,
 		    colors  = control.params.choices,
 		    keys    = Object.keys( colors ),
 		    value   = this.params.value,
-		    target  = control.container.find( '.iris-target' ),
-		    i       = 0,
+			i       = 0,
+		    target,
 		    irisInput,
 		    irisPicker;
 
-		// Proxy function that handles changing the individual colors
-		function kirkiMulticolorChangeHandler( control, value, subSetting ) {
-
-			var picker = control.container.find( '.multicolor-index-' + subSetting ),
-			    args   = {
-					target: target[0],
-					change: function() {
-
-						// Color controls require a small delay.
-						setTimeout( function() {
-
-							// Set the value.
-							control.saveValue( subSetting, picker.val() );
-
-							// Trigger the change.
-							control.container.find( '.multicolor-index-' + subSetting ).trigger( 'change' );
-						}, 100 );
-					}
-			    };
-
-			if ( _.isObject( colors.irisArgs ) ) {
-				_.each( colors.irisArgs, function( irisValue, irisKey ) {
-					args[ irisKey ] = irisValue;
-				} );
-			}
-
-			// Did we change the value?
-			picker.wpColorPicker( args );
-		}
+		control.addHTML();
+  		target = control.container.find( '.iris-target' );
 
 		// Colors loop
 		while ( i < Object.keys( colors ).length ) {
 
-			kirkiMulticolorChangeHandler( this, value, keys[ i ] );
+			control.kirkiMulticolorChangeHandler( this, value, keys[ i ] );
 
 			// Move colorpicker to the 'iris-target' container div
 			irisInput  = control.container.find( '.wp-picker-container .wp-picker-input-wrap' ),
@@ -63,6 +27,36 @@ wp.customize.controlConstructor['kirki-multicolor'] = wp.customize.Control.exten
 
 			i++;
 		}
+	},
+
+	// Proxy function that handles changing the individual colors
+	kirkiMulticolorChangeHandler: function( control, value, subSetting ) {
+
+		var colors  = control.params.choices,
+		    target = control.container.find( '.iris-target' ),
+		    picker = control.container.find( '.multicolor-index-' + subSetting ),
+		    args   = {
+				target: target[0],
+				change: function() {
+					setTimeout( function() {
+
+						// Set the value.
+						control.kirkiSetValue( picker.val(), subSetting );
+
+						// Trigger the change.
+						control.container.find( '.multicolor-index-' + subSetting ).trigger( 'change' );
+					}, 100 );
+				}
+			};
+
+		if ( _.isObject( colors.irisArgs ) ) {
+			_.each( colors.irisArgs, function( irisValue, irisKey ) {
+				args[ irisKey ] = irisValue;
+			} );
+		}
+
+		// Did we change the value?
+		picker.wpColorPicker( args );
 	},
 
 	addHTML: function() {
@@ -82,26 +76,8 @@ wp.customize.controlConstructor['kirki-multicolor'] = wp.customize.Control.exten
 			} );
 		html += '</div>';
 		html += '<div class="iris-target"></div>';
-		html += '<input class="multicolor-hidden-value" type="hidden" value=\'' + JSON.stringify( control.params.value ) + '\' ' + control.params.link + '>';
 
 		control.container.html( html );
-	},
-
-	/**
-	 * Saves the value.
-	 */
-	saveValue: function( property, value ) {
-
-		'use strict';
-
-		var control   = this,
-		    input     = control.container.find( '.multicolor-hidden-value' ),
-		    valueJSON = jQuery( input ).val(),
-		    valueObj  = JSON.parse( valueJSON );
-
-		valueObj[ property ] = value;
-		jQuery( input ).attr( 'value', JSON.stringify( valueObj ) ).trigger( 'change' );
-		control.setting.set( valueObj );
 	},
 
 	kirkiSetControlValue: function( value ) {
