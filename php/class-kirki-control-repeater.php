9c3de17a10778a3fca00bccd7774e1b393a381b6
Aristeides Stathopoulos <aristath@gmail.com>
2017-08-21 01:41:03 +0300

    Delete repeater fields. Needs to be refactored.

--- php/class-kirki-control-repeater.php
+++ php/class-kirki-control-repeater.php
@@ -25,7 +25,7 @@ class Kirki_Control_Repeater extends Kirki_Control_Base {
 	 * @access public
 	 * @var string
 	 */
-	public $type = 'repeater';
+	public $type = 'kirki-repeater';
 
 	/**
 	 * The fields that each container row will contain.
@@ -52,129 +52,6 @@ class Kirki_Control_Repeater extends Kirki_Control_Base {
 	public $row_label = array();
 
 	/**
-	 * Constructor.
-	 * Supplied `$args` override class property defaults.
-	 * If `$args['settings']` is not defined, use the $id as the setting ID.
-	 *
-	 * @param WP_Customize_Manager $manager Customizer bootstrap instance.
-	 * @param string               $id      Control ID.
-	 * @param array                $args    {@see WP_Customize_Control::__construct}.
-	 */
-	public function __construct( $manager, $id, $args = array() ) {
-
-		parent::__construct( $manager, $id, $args );
-
-		// Set up defaults for row labels.
-		$this->row_label = array(
-			'type' => 'text',
-			'value' => esc_attr__( 'row', 'kirki' ),
-			'field' => false,
-		);
-
-		// Validate row-labels.
-		$this->row_label( $args );
-
-		if ( empty( $this->button_label ) ) {
-			/* translators: %s represents the label of the row. */
-			$this->button_label = sprintf( esc_attr__( 'Add new %s', 'kirki' ), $this->row_label['value'] );
-		}
-
-		if ( empty( $args['fields'] ) || ! is_array( $args['fields'] ) ) {
-			$args['fields'] = array();
-		}
-
-		// An array to store keys of fields that need to be filtered.
-		$filter_media_fields = array();
-
-		foreach ( $args['fields'] as $key => $value ) {
-			if ( ! isset( $value['default'] ) ) {
-				$args['fields'][ $key ]['default'] = '';
-			}
-			if ( ! isset( $value['label'] ) ) {
-				$args['fields'][ $key ]['label'] = '';
-			}
-			$args['fields'][ $key ]['id']      = $key;
-
-			// We check if the filed is an uploaded media ( image , file, video, etc.. ).
-			if ( isset( $value['type'] ) ) {
-				switch ( $value['type'] ) {
-					case 'image':
-					case 'cropped_image':
-					case 'upload':
-						// We add it to the list of fields that need some extra filtering/processing.
-						$filter_media_fields[ $key ] = true;
-						break;
-
-					case 'dropdown-pages':
-						// If the field is a dropdown-pages field then add it to args.
-						$dropdown = wp_dropdown_pages(
-							array(
-								'name'              => '',
-								'echo'              => 0,
-								'show_option_none'  => esc_attr__( 'Select a Page', 'kirki' ),
-								'option_none_value' => '0',
-								'selected'          => '',
-							)
-						);
-						// Hackily add in the data link parameter.
-						$dropdown = str_replace( '<select', '<select data-field="' . esc_attr( $args['fields'][ $key ]['id'] ) . '"' . $this->get_link(), $dropdown );
-						$args['fields'][ $key ]['dropdown'] = $dropdown;
-						break;
-				}
-			}
-		} // End foreach().
-
-		$this->fields = $args['fields'];
-
-		// Now we are going to filter the fields.
-		// First we create a copy of the value that would be used otherwise.
-		$this->filtered_value = $this->value();
-
-		if ( is_array( $this->filtered_value ) && ! empty( $this->filtered_value ) ) {
-
-			// We iterate over the list of fields.
-			foreach ( $this->filtered_value as &$filtered_value_field ) {
-
-				if ( is_array( $filtered_value_field ) && ! empty( $filtered_value_field ) ) {
-
-					// We iterate over the list of properties for this field.
-					foreach ( $filtered_value_field as $key => &$value ) {
-
-						// We check if this field was marked as requiring extra filtering (in this case image, cropped_images, upload).
-						if ( array_key_exists( $key, $filter_media_fields ) ) {
-
-							// What follows was made this way to preserve backward compatibility.
-							// The repeater control use to store the URL for images instead of the attachment ID.
-							// We check if the value look like an ID (otherwise it's probably a URL so don't filter it).
-							if ( is_numeric( $value ) ) {
-
-								// "sanitize" the value.
-								$attachment_id = (int) $value;
-
-								// Try to get the attachment_url.
-								$url = wp_get_attachment_url( $attachment_id );
-
-								$filename = basename( get_attached_file( $attachment_id ) );
-
-								// If we got a URL.
-								if ( $url ) {
-
-									// 'id' is needed for form hidden value, URL is needed to display the image.
-									$value = array(
-										'id'  => $attachment_id,
-										'url' => $url,
-										'filename' => $filename,
-									);
-								}
-							}
-						}
-					}
-				}
-			} // End foreach().
-		} // End if().
-	}
-
-	/**
 	 * Refresh the parameters passed to the JavaScript via JSON.
 	 *
 	 * @access public
@@ -182,21 +59,8 @@ class Kirki_Control_Repeater extends Kirki_Control_Base {
 	public function to_json() {
 		parent::to_json();
 
-		$this->json['default'] = ( isset( $this->default ) ) ? $this->default : $this->setting->default;
-		$this->json['output']  = $this->output;
-		$this->json['value']   = $this->value();
-		$this->json['choices'] = $this->choices;
-		$this->json['link']    = $this->get_link();
-		$this->json['id']      = $this->id;
-
-		$this->json['inputAttrs'] = '';
-		foreach ( $this->input_attrs as $attr => $value ) {
-			$this->json['inputAttrs'] .= $attr . '="' . esc_attr( $value ) . '" ';
-		}
-
-		$fields = $this->fields;
-
-		$this->json['fields'] = $fields;
+		$this->json['default']   = ( isset( $this->default ) ) ? $this->default : $this->setting->default;
+		$this->json['fields']    = $this->fields;
 		$this->json['row_label'] = $this->row_label;
 
 		// If filtered_value has been set and is not empty we use it instead of the actual value.
@@ -204,133 +68,5 @@ class Kirki_Control_Repeater extends Kirki_Control_Base {
 			$this->json['value'] = $this->filtered_value;
 		}
 		$this->json['value'] = apply_filters( "kirki/controls/repeater/value/{$this->id}", $this->json['value'] );
-	}
-
-	/**
-	 * Enqueue control related scripts/styles.
-	 *
-	 * @access public
-	 */
-	public function enqueue() {
-
-		// If we have a color picker field we need to enqueue the WordPress Color Picker style and script.
-		if ( is_array( $this->fields ) && ! empty( $this->fields ) ) {
-			foreach ( $this->fields as $field ) {
-				if ( isset( $field['type'] ) ) {
-
-					// Some field-types require extra scripts.
-					switch ( $field['type'] ) {
-						case 'color':
-							wp_enqueue_script( 'wp-color-picker' );
-							wp_enqueue_style( 'wp-color-picker' );
-							break;
-						case 'select':
-						case 'dropdown-pages':
-							wp_enqueue_script( 'select2', kirki_controls()->get_url( 'vendor/select2/js/select2.full.js' ), array( 'jquery' ), '4.0.3', true );
-							wp_enqueue_style( 'select2', kirki_controls()->get_url( 'vendor/select2/css/select2.css' ), array(), '4.0.3' );
-							break;
-					}
-				}
-			}
-		}
-
-		wp_enqueue_script( 'kirki-repeater', kirki_controls()->get_url( 'js/repeater.js' ), array( 'jquery', 'customize-base', 'jquery-ui-core', 'jquery-ui-sortable' ), false, true );
-		wp_enqueue_style( 'kirki-styles', kirki_controls()->get_url( 'css/styles.css' ), null );
-	}
-
-	/**
-	 * Render the control's content.
-	 * Allows the content to be overriden without having to rewrite the wrapper in $this->render().
-	 *
-	 * @access protected
-	 */
-	protected function render_content() {
-		?>
-		<label>
-			<?php if ( ! empty( $this->label ) ) : ?>
-				<span class="customize-control-title"><?php echo esc_html( $this->label ); ?></span>
-			<?php endif; ?>
-			<?php if ( ! empty( $this->description ) ) : ?>
-				<span class="description customize-control-description"><?php echo wp_kses_post( $this->description ); ?></span>
-			<?php endif; ?>
-			<input type="hidden" {{{ data.inputAttrs }}} value="" <?php echo wp_kses_post( $this->get_link() ); ?> />
-		</label>
-
-		<ul class="repeater-fields"></ul>
-
-		<?php if ( isset( $this->choices['limit'] ) ) : ?>
-			<?php // @codingStandardsIgnoreLine ?>
-			<?php /* translators: %s represents the number of rows we're limiting the repeater to allow. */ ?>
-			<p class="limit"><?php printf( esc_attr__( 'Limit: %s rows', 'kirki' ), esc_html( $this->choices['limit'] ) ); ?></p>
-		<?php endif; ?>
-		<button class="button-secondary repeater-add"><?php echo esc_html( $this->button_label ); ?></button>
-
-		<?php
-
-		$this->repeater_js_template();
-
-	}
-
-	/**
-	 * An Underscore (JS) template for this control's content (but not its container).
-	 * Class variables for this control class are available in the `data` JS object.
-	 *
-	 * @access public
-	 */
-	public function repeater_js_template() {
-		?>
-		<script type="text/html" class="customize-control-repeater-content">
-			<# var field; var index = data.index; #>
-
-			<li class="repeater-row minimized" data-row="{{{ index }}}">
-
-				<div class="repeater-row-header">
-					<span class="repeater-row-label"></span>
-					<i class="dashicons dashicons-arrow-down repeater-minimize"></i>
-				</div>
-				<div class="repeater-row-content">
-					<# _.each( data, function( field, i ) { #>
-
-						<div class="repeater-field repeater-field-{{{ field.type }}}">
-
-						</div>
-					<# } ); #>
-					<button type="button" class="button-link repeater-row-remove"><?php esc_attr_e( 'Remove', 'kirki' ); ?></button>
-				</div>
-			</li>
-		</script>
-		<?php
-	}
-
-	/**
-	 * Validate row-labels.
-	 *
-	 * @access protected
-	 * @since 3.0.0
-	 * @param array $args {@see WP_Customize_Control::__construct}.
-	 */
-	protected function row_label( $args ) {
-
-		// Validating args for row labels.
-		if ( isset( $args['row_label'] ) && is_array( $args['row_label'] ) && ! empty( $args['row_label'] ) ) {
-
-			// Validating row label type.
-			if ( isset( $args['row_label']['type'] ) && ( 'text' === $args['row_label']['type'] || 'field' === $args['row_label']['type'] ) ) {
-				$this->row_label['type'] = $args['row_label']['type'];
-			}
-
-			// Validating row label type.
-			if ( isset( $args['row_label']['value'] ) && ! empty( $args['row_label']['value'] ) ) {
-				$this->row_label['value'] = esc_attr( $args['row_label']['value'] );
-			}
-
-			// Validating row label field.
-			if ( isset( $args['row_label']['field'] ) && ! empty( $args['row_label']['field'] ) && isset( $args['fields'][ esc_attr( $args['row_label']['field'] ) ] ) ) {
-				$this->row_label['field'] = esc_attr( $args['row_label']['field'] );
-			} else {
-				// If from field is not set correctly, making sure standard is set as the type.
-				$this->row_label['type'] = 'text';
-			}
-		}
 	}
 }
