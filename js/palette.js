ded76a499efba9d429ce423c9261a42c3c512de3
Aristeides Stathopoulos <aristath@gmail.com>
2017-08-20 19:56:39 +0300

    Convert palette control template.

--- js/palette.js
+++ js/palette.js
@@ -1,6 +1,31 @@
 /* global wp, _ */
 wp.customize.controlConstructor['kirki-palette'] = wp.customize.kirkiDynamicControl.extend( {
 
+	addHTML: function() {
+		var control = this,
+		    html    = '';
+
+		if ( ! control.params.choices ) {
+			return;
+		}
+		html += '<span class="customize-control-title">' + control.params.label + '</span>';
+		html += '<span class="description customize-control-description">' + control.params.description + '</span>';
+		html += '<div id="input_' + control.params.id + '" class="buttonset">';
+		_.each( control.params.choices, function( colors, key ) {
+			html += '<input ' + control.params.inputAttrs + ' type="radio" value="' + key + '" name="_customize-palette-' + control.id + '" id="' + control.id + key + '" ' + control.params.link + ( control.params.value === key ? ' checked' : '' ) + '>';
+				html += '<label for="' + control.id + key + '">';
+					_.each( colors, function( color ) {
+						html += '<span style="background:' + color + '">' + color + '</span>';
+					} );
+				html += '</label>';
+			html += '</input>';
+		} );
+		html += '</div>';
+
+		control.container.html( html );
+
+	},
+
 	kirkiSetControlValue: function( value ) {
 		var control = this;
 		jQuery( control.container.find( 'input[value="' + value + '"]' ) ).prop( 'checked', true );
--- php/class-kirki-control-palette.php
+++ php/class-kirki-control-palette.php
@@ -36,37 +36,4 @@ class Kirki_Control_Palette extends Kirki_Control_Base {
 	protected function kirki_script_dependencies() {
 		return array( 'jquery-ui-button' );
 	}
-
-	/**
-	 * An Underscore (JS) template for this control's content (but not its container).
-	 *
-	 * Class variables for this control class are available in the `data` JS object;
-	 * export custom variables by overriding {@see WP_Customize_Control::to_json()}.
-	 *
-	 * @see WP_Customize_Control::print_template()
-	 *
-	 * @access protected
-	 */
-	protected function content_template() {
-		?>
-		<# if ( ! data.choices ) { return; } #>
-		<span class="customize-control-title">
-			{{ data.label }}
-		</span>
-		<# if ( data.description ) { #>
-			<span class="description customize-control-description">{{{ data.description }}}</span>
-		<# } #>
-		<div id="input_{{ data.id }}" class="buttonset">
-			<# for ( key in data.choices ) { #>
-				<input {{{ data.inputAttrs }}} type="radio" value="{{ key }}" name="_customize-palette-{{ data.id }}" id="{{ data.id }}{{ key }}" {{{ data.link }}}<# if ( data.value == key ) { #> checked<# } #>>
-					<label for="{{ data.id }}{{ key }}">
-						<# for ( color in data.choices[ key ] ) { #>
-							<span style='background: {{ data.choices[ key ][ color ] }}'>{{ data.choices[ key ][ color ] }}</span>
-						<# } #>
-					</label>
-				</input>
-			<# } #>
-		</div>
-		<?php
-	}
 }
