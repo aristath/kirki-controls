5c45a036a2990ea46157dc507a4ff25213ed41b6
Aristeides Stathopoulos <aristath@gmail.com>
2017-08-22 02:47:25 +0300

    better header for rows

--- js/kirki.js
+++ js/kirki.js
@@ -686,8 +686,14 @@ var kirki = {
 		repeaterControlRow: function( control, value, rowKey ) {
 			var rowTemplate = '';
 
-			rowTemplate = '<li class="field-template">';
-			rowTemplate += '<div class="row-header">Row Title<span class="dashicons dashicons-move"></span><span class="dashicons dashicons-trash"></span></div>';
+			rowTemplate = '<li class="row-template">';
+			rowTemplate += '<div class="row-header">';
+				rowTemplate += 'Row Title';
+				rowTemplate += '<div class="repeater-row-actions">';
+					rowTemplate += '<span class="repeater-row-move-button dashicons dashicons-move"></span>';
+					rowTemplate += '<span class="repeater-row-remove-button dashicons dashicons-trash"></span>';
+				rowTemplate += '</div>';
+			rowTemplate += '</div>';
 
 
 				// Go through each field.
