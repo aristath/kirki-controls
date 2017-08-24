300191d1694c3db46dd2334ea250c0a2671614c4
Aristeides Stathopoulos <aristath@gmail.com>
2017-08-25 01:33:59 +0300

    remove gradient control.

--- php/class-kirki-controls-bootstrap.php
+++ php/class-kirki-controls-bootstrap.php
@@ -149,7 +149,6 @@ class Kirki_Controls_Bootstrap {
 			'kirki-dimensions'      => 'Kirki_Control_Dimensions',
 			'kirki-editor'          => 'Kirki_Control_Editor',
 			'kirki-fontawesome'     => 'Kirki_Control_FontAwesome',
-			'kirki-gradient'        => 'Kirki_Control_Gradient',
 			'kirki-image'           => 'Kirki_Control_Image',
 			'kirki-multicolor'      => 'Kirki_Control_Multicolor',
 			'kirki-multicheck'      => 'Kirki_Control_MultiCheck',
